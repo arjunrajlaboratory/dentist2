0f32672a13847ef96b3bbcc620b5365b27fed3b0
Benjamin Emert <benjamin.emert@gmail.com>
2021-02-05 12:26:58 -0500

    version of cellTable that includes channels
    Version of cellTable that includes uniqe cellID for each cell x channel

--- cellTable.m
+++ cellTable.m
@@ -3,19 +3,23 @@ classdef cellTable < handle
     properties (Access = public)
         
         cells
-        masks
-        
+        masks %Do we want to mask cells? 
+        spotChannels
         minNucleusSize = 1000; % Set method should probably call findCells
 
     end
     
     methods
         
-        function p = cellTable(varargin)
-            if nargin == 0
+        function p = cellTable(scanObject, varargin)
+            if nargin == 1
+                channels = scanObject.channels;
+                p.spotChannels = channels(~ismember(channels,["dapi","trans"]));
                 fprintf('New Table\n');
-                p.cells = cell2table(cell(0,5), 'VariableNames', {'cellID', 'x', 'y', 'status', 'area'}); 
-            else
+                p.cells = cell2table(cell(0,8), 'VariableNames', {'cellID', 'x', 'y', 'channel', 'spotCount', 'status', 'maskID', 'nucleusArea'}); 
+            elseif nargin == 2
+                channels = scanObject.channels;
+                p.spotChannels = channels(~ismember(channels,["dapi","trans"]));
                 fprintf('Loading Table\n');
                 p.cells = readtable(varargin{1},'TextType','string');
             end
@@ -31,14 +35,29 @@ classdef cellTable < handle
             rp = rp(idx);
             centroids = [rp.Centroid];
             centroids = round(reshape(centroids,2,[])');
+            centroids = single(centroids);
+            cellCount = height(centroids);
+            centroids = repmat(centroids, numel(p.spotChannels),1);
             
-            area = [rp.Area];
-            status = ones(size(centroids, 1),1);
-            
-            
-            p.cells = table((1:size(centroids, 1))',centroids(:,2),centroids(:,1),status, area', 'VariableNames', {'cellID', 'x', 'y', 'status', 'area'});
+            area = single([rp.Area]);
+            area = repmat(area', numel(p.spotChannels),1);
+           
+            status = true(height(centroids), 1);
+            spots = single(zeros(height(centroids), 1));
+            maskID = single(zeros(height(centroids), 1));
+            channel = [];
+            for i = 1:numel(p.spotChannels)
+                channel = [channel; repmat(p.spotChannels(i), cellCount, 1)];
+            end
+
+            p.cells = table((1:height(centroids))',centroids(:,2),centroids(:,1), channel, spots, status, maskID, area,...
+                'VariableNames', {'cellID', 'x', 'y', 'channel', 'spotCount', 'status', 'maskID', 'nucleusArea'});
         end
         
+        function p = spotsPerCell(p, spotTable)
+            spotCounts = groupsummary(spotTable, {'channel', 'nearestCellID'});
+            %ADD CODE HERE
+        end
         function outCells = getCellsInRect(p,rect) %rect specified as [x y nrows ncols]
             
             outCells = p.cells(p.cells.status==1,:);
@@ -51,6 +70,7 @@ classdef cellTable < handle
                 
         function set.minNucleusSize(p,area)
             p.minNucleusSize = area;
+            %UPDATE CELLS
         end
         
         function p = addCell(p, x, y, status, area)
@@ -60,19 +80,21 @@ classdef cellTable < handle
                 newCell = {maxID+1, x, y, status, area};
                 tempCells = [tempCells;newCell];
                 p.cells = tempCells;
+                % UPDATE SPOTS?
             else
-                p.cells(1,:) = {1, x, y, status, area};
+                p.cells(1,:) = {1, x, y, status, 0, 0, area};
             end
         end
         
         function p = removeCell(p, x, y)
             if ~isempty(p.cells)
                 tempCells = p.cells;
-                [idx, dist] = dsearchn(tempCells{:,{'x','y'}}, [x, y]);
+                [idx, dist] = knnsearch(tempCells{:,{'x','y'}}, [x, y], 'K', 1, 'Distance', 'euclidean');
                 if dist < 40 %make sure that the query point is near a cell
                     tempCells(idx,:) = [];
                     p.cells = tempCells;
                 end
+                % UPDATE SPOTS
             else
                 disp("cells is empty.")
             end
@@ -89,7 +111,6 @@ classdef cellTable < handle
                 fprintf("cells is empty. Run findCells and try again")
             end
         end
-
         
     end
     
